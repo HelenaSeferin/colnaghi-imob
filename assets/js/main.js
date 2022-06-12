74de4a117df43ed2ee53bd7182da75f9bc99a01e
unknown <helena.seferin@hotmail.com>
2022-06-12 00:13:35 -0300

    first blood

---
+++ assets/js/main.js
@@ -0,0 +1,322 @@
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+ 
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle ', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  on('click', '.bi-x ', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      setTimeout(function() {
+      preloader.remove()
+    }, 1000);
+    });
+  }
+
+  /**
+   * Initiate  glightbox 
+   */
+  // const glightbox = GLightbox({
+  //   selector: '.glightbox'
+  // });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    slidesPerView: 3, 
+    centeredSlides: true,
+    autoplay: false,
+    
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+      
+
+  /**
+   * FAVORITAR
+   */
+
+  $(".heart.fa").click(function() {
+    $(this).toggleClass("fa-heart fa-heart-o");
+  });
+
+  /**
+   * BOTÃO SUCESSO ENVIO NEWS
+   */
+
+  $( ".btn-news" ).click(function() {
+    $( ".form-sucess" ).first().fadeIn( "slow" );
+    $( ".form-news" ).hide();
+  });
+
+  $( ".form-new" ).click(function() {
+    $( ".form-news" ).first().fadeIn( "slow" );
+    $( ".form-sucess" ).hide();
+  });
+
+  $(document).ready(function() { 
+    $(window).bind("resize", function () {
+      if ($(this).width() <= 1024) {
+        $('.gallery').addClass('mobile-slide');
+        
+        $('.mobile-slide').slick({
+          centerMode: true,
+          slidesToShow: 3, variableWidth: true,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 540,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 3
+              }
+            }
+          ]
+        });         
+          
+      } else {
+          $('.gallery').removeClass('mobile-slide');
+      }
+
+      if ($(this).width() <= 768) {
+        $('.district-item').addClass('district-slide');
+        
+        $('.district-slide').slick({
+          centerMode: true,
+          slidesToShow: 3, variableWidth: true,
+          responsive: [
+          
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              slidesToShow: 2
+            }
+          }
+          ]
+      });   
+    } else {
+        $('.district-item').removeClass('district-slide');
+    }
+    }).resize();
+  })
+ 
+
+})()
---
+++ hf.js
@@ -0,0 +1,178 @@
+class Header extends HTMLElement {
+	connectedCallback() {
+      this.innerHTML = `
+      <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">  
+          <h1 class="logo me-auto">
+		  	<a href="index.html">
+			  <img src="assets/img/logo.png" alt="">
+			 </a>
+		</h1>	  
+          <nav id="navbar" class="navbar">    
+            <ul class="nav-mobile">
+              <li class="bi-list bi-x desk-none"></li>
+              <li class="desk-none">
+                <a href="index.html" class="logo-mobile">
+                <img src="assets/img/logo.png" alt=""></a>
+              </li>
+              <li class="dropdown dropdown-mobile">
+                <a href="#">
+                  <span>Sobre nós</span> 
+                  <i class="bi bi-chevron-down"></i>
+                  </a>
+                <ul>
+                  <li>
+                    <a href="#">Sobre nós</a>
+                  </li>                  
+                  <li>
+                    <a href="#">Trabalhe conosco</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a class="nav-link" href="#">Anuncie seu imóvel</a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">Blog</a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">Lançamentos</a>
+              </li>
+              <li>
+                <a class="nav-link" href="#">Tour Virtual</a></l
+              i>			
+              <li>
+                <a class="nav-link" href="#">Meus Favoritos</a>
+              </li>
+              <li>
+                <a class="getstarted" href="#">Login</a>
+              </li>
+            </ul>
+                  
+          </nav> 
+          <div class=" mobile-nav-toggle">
+          <span></span>
+          <span class="menu-small"></span>
+          <span></span>
+        </div>  
+        </div>
+      </header>
+		  `
+	  }
+  }
+  
+  //Footer
+  
+  class Footer extends HTMLElement {
+	connectedCallback() {
+	  this.innerHTML = 
+		`
+		<footer id="footer">
+			<div class="footer-top">
+				<div class="container">
+					<div class="row">	
+						<div class="col-lg-4 col-md-6 text">
+							<p>Nós trabalhamos incansavelmente para oferecer uma experiência intensa e apaixonante na busca pelo novo <br>lar.  <br><br>
+							<strong>Conheça o nosso jeito!</strong></p>
+						</div>	
+						<div class="col-lg-5 col-md-6 links">
+							<div class="social-links">
+								<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+								<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+								<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+								<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+							</div>
+							<div class="address">
+								<h4>Our Rua Dona Laura, 233 - Rio Branco, Porto Alegre - RS, <br> 90430-091
+							</h4>
+							<ul class="contacts">
+								<li><i class="bx ri-phone-line"></i> <a href="#" class="link">51 3331.2944</a></li>
+								<li><i class="bx bxl-whatsapp"></i> <a href="#" class="link">51 99962.4252</a></li>
+							</ul>
+						</div>           
+					</div>	       	
+					<div class="col-lg-3 col-md-6 footer-links">
+						<h3>Política de Privacidade</h3>				
+						<div class="sitemap">
+							<div class="row">
+							<ul class="col-6">
+								<li><a href="#" class="link">Buscar Imóveis</a></li>
+								<li><a href="#" class="link">Meus favoritos</a></li>            
+								<li><a href="#" class="link">Blog</a></li>
+							</ul>
+							<ul class="col-6"> 
+								<li><a href="#" class="link">Fale conosco</a></li>
+								<li><a href="#" class="link">Construtoras</a></li>         
+							</ul>
+							</div>         
+						</div>         				
+					</div>	
+					</div>
+				</div>
+			</div>
+	
+		<div class="container footer-bottom clearfix">
+      <hr>
+		  <div class="copyright">
+        Colnaghi Imóveis <br>
+        Imobiliária Porto Alegre com Imóveis Alto Padrão <br>
+        CRECI-SP 22.712		  
+      </div>
+		
+		</div>
+	  </footer>  
+		`
+	  
+	}
+  }
+  customElements.define('main-header', Header);
+  customElements.define('main-footer', Footer);
+
+  const slider = document.querySelector('.box-propierties-slide');
+	let isDown = false;
+	let startX;
+	let scrollLeft;
+
+	slider.addEventListener('mousedown', e => {
+	isDown = true;
+	slider.classList.add('active');
+	startX = e.pageX - slider.offsetLeft;
+	scrollLeft = slider.scrollLeft;
+	});
+	slider.addEventListener('mouseleave', _ => {
+	isDown = false;
+	slider.classList.remove('active');
+	});
+	slider.addEventListener('mouseup', _ => {
+	isDown = false;
+	slider.classList.remove('active');
+	});
+	slider.addEventListener('mousemove', e => {
+	if (!isDown) return;
+	e.preventDefault();
+	const x = e.pageX - slider.offsetLeft;
+	const SCROLL_SPEED = 2;
+	const walk = (x - startX) * SCROLL_SPEED;
+	slider.scrollLeft = scrollLeft - walk;
+});
+
+
+// CLONE DE ITENS DO CARROSSEL - REMOVER DEPOIS DA INTEGRAÇÃO
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.box-properties'), 30, true);
+
+// CLONE DE ITENS DO BAIRRO - REMOVER DEPOIS DA INTEGRAÇÃO
+function multiplyNode(node, count, deep) {
+    for (var i = 0, copy; i < count - 1; i++) {
+        copy = node.cloneNode(deep);
+        node.parentNode.insertBefore(copy, node);
+    }
+}
+
+multiplyNode(document.querySelector('.district-box'), 20, true);
